bdb4ed9f668ff55b8430b84df987cdd5dc8cd52a
Heritage <ireanuoluwah@gmail.com>
2025-04-02 00:06:59 +0100

    done with first 2 pages, Maybe

--- src/components/AddExpense.tsx
+++ src/components/AddExpense.tsx
@@ -1,17 +1,39 @@
-import { TextInput, Button, Box } from "@mantine/core";
+import {Button, Box } from "@mantine/core";
+import "../index.css";
 const AddExpense = () => {
   return (
     <>
-      <form action="">
-        <TextInput
-          label="Name"
-          placeholder="Name"
-        />
-        <TextInput label="Amount" placeholder="Enter amount" />
-        <TextInput label="Category" placeholder="Enter category" />
-        <TextInput label="Date" placeholder="Enter date"  />
-        <Box style={{display:'flex', justifyContent:'center'}}>
-            <Button style={{position:'absolute',bottom:'10px', width:'85%', height:'45px', borderRadius:'13px'}} type="submit">Add Expense</Button>
+      <form style={{ display: "flex", flexDirection: "column", gap:'25px'}} action="">
+        <Box>
+          <label>Name</label>
+          <input className="modalInput" placeholder="Name" />
+        </Box>
+        <Box>
+          <label>Amount</label>
+          <input className="modalInput" placeholder="Enter amount" />
+        </Box>
+        <Box>
+          <label>Category</label>
+          <input className="modalInput" placeholder="Category" />
+        </Box>
+        <Box>
+          <label>Date</label>
+          <input type="select" className="modalInput" placeholder="Enter date" />
+        </Box>
+
+        <Box style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            style={{
+              position: "absolute",
+              bottom: "10px",
+              width: "85%",
+              height: "45px",
+              borderRadius: "13px",
+            }}
+            type="submit"
+          >
+            Add Expense
+          </Button>
         </Box>
       </form>
     </>
